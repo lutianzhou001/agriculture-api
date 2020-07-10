08bfea2f820c377df29dd7d75f73eb3cc1a781e9
yooml <947801604@qq.com>
2020-07-10 22:16:01 +0800

    delete explorer

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,6 @@ import { AppService } from './app.service';
 import { FarmingModule } from './farming/farming.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { default as config } from './config';
-import { ContractModule } from './contract/contract.module';
-import { ExplorerModule } from './explorer/explorer.module';
 import { Web3Module } from './web3/web3.module';
 
 const userString = config.db.user && config.db.pass ? (config.db.user + ':' + config.db.pass + '@') : '';
@@ -15,8 +13,6 @@ const authSource = config.db.authSource ? ('?authSource=' + config.db.authSource
   imports: [
     MongooseModule.forRoot('mongodb://' + userString + config.db.host + ':' + (config.db.port || '27017') + '/' + config.db.database + authSource, { useFindAndModify: false }),
     FarmingModule,
-    //ContractModule,
-    //ExplorerModule,
     Web3Module],
   controllers: [AppController],
   providers: [AppService],
